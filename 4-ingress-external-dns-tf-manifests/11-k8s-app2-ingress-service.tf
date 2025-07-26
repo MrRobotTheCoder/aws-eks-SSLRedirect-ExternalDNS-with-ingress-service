0ef038c88d78b2fb299c37afdabd1abd796a8aa3
Abinash Sethi <sethiabinash123@gmail.com>
2025-07-26 19:42:30 -0400

    added ingress group

--- 4-ingress-external-dns-tf-manifests/11-k8s-app2-ingress-service.tf
+++ 4-ingress-external-dns-tf-manifests/11-k8s-app2-ingress-service.tf
@@ -1,10 +1,10 @@
 # Kubernetes Service Manifest (Type: Load Balancer)
-resource "kubernetes_ingress_v1" "ingress" {
+resource "kubernetes_ingress_v1" "ingress_app2" {
   metadata {
-    name = "ingress-ssl-demo"
+    name = "app2-ingress"
     annotations = {
       # Load Balancer Name
-      "alb.ingress.kubernetes.io/load-balancer-name" = "ingress-ssl-demo"
+      "alb.ingress.kubernetes.io/load-balancer-name" = "ingress-groups-demo"
       # Ingress Core Settings
       "alb.ingress.kubernetes.io/scheme" = "internet-facing"
       # Health Check Settings
@@ -20,41 +20,23 @@ resource "kubernetes_ingress_v1" "ingress" {
       # Option-1: Using Terraform jsonencode Function
       "alb.ingress.kubernetes.io/listen-ports" = jsonencode([{"HTTPS" = 443}, {"HTTP" = 80}])
       # Option-2: Using Terraform File Function      
-      #"alb.ingress.kubernetes.io/listen-ports" = file("${path.module}/listen-ports/listen-ports.json")   
-      "alb.ingress.kubernetes.io/certificate-arn" =  "${aws_acm_certificate.acm_cert.arn}"
+      #"alb.ingress.kubernetes.io/listen-ports" = file("${path.module}/listen-ports/listen-ports.json")
+      "alb.ingress.kubernetes.io/certificate-arn" =  "${aws_acm_certificate.acm_cert.arn}"    
       #"alb.ingress.kubernetes.io/ssl-policy" = "ELBSecurityPolicy-TLS-1-1-2017-01" #Optional (Picks default if not used)    
       # SSL Redirect Setting
       "alb.ingress.kubernetes.io/ssl-redirect" = 443
       # External DNS - For creating a Record Set in Route53
-      "external-dns.alpha.kubernetes.io/hostname" = "tfdnstest901.hellosaanvika.com, tfdnstest902.hellosaanvika.com"
+      "external-dns.alpha.kubernetes.io/hostname" = "tfingress-groups-demo102.hellosaanvika.com"
+      # Ingress Groups
+      "alb.ingress.kubernetes.io/group.name" = "myapps.web"
+      "alb.ingress.kubernetes.io/group.order" = 20
     }    
   }
-  spec {
-    ingress_class_name = "my-aws-ingress-class" # Ingress Class            
-    default_backend {
-      service {
-        name = kubernetes_service_v1.myapp3_np_service.metadata[0].name
-        port {
-          number = 80
-        }
-      }
-    }
 
+  spec {
+    ingress_class_name = "my-aws-ingress-class" # Ingress Class        
     rule {
       http {
-        path {
-          backend {
-            service {
-              name = kubernetes_service_v1.myapp1_np_service.metadata[0].name
-              port {
-                number = 80
-              }
-            }
-          }
-          path = "/app1"
-          path_type = "Prefix"
-        }
-
         path {
           backend {
             service {
@@ -69,5 +51,6 @@ resource "kubernetes_ingress_v1" "ingress" {
         }
       }
     }
+
   }
 }
